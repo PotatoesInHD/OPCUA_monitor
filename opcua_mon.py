aa7667cc613f8e839cf35526f1261bef2ce50496
potates <potates@mashed.com>
2026-09-10 21:18:47 -0400

    molded some file path logic from the ai agen guided project. added some more stuff todo list for more filepath finding improvements

--- opcua_mon.py
+++ opcua_mon.py
@@ -13,7 +13,7 @@ from mdb_parser import MDBParser, MDBTable
 CONN_MONITOR_NODE = "ns=2;i=4"
 WRITE_DETECTED_NODE = "ns=2;i=5"
 FROM_PLC_WRITE_CMPLT_NODE = "ns=2;i=6"
-MDB_DIR_PATH  = "/home/testies/opcuaproj/"
+MDB_DIR_PATH  = "."
 OPCUA_URL = "opc.tcp://10.0.0.114:4840"
 
 
@@ -41,20 +41,39 @@ def get_mdb_filename() -> str:
     mdb_filename = date.strftime(f"%{pad}m-%{pad}d-%Y-BS.mdb")
     return mdb_filename
 
+def get_file_path(directory: str, file_name: str) -> str:
+    #https://pyinstaller.org/en/stable/runtime-information.html
+    #look into this when using pyinstaller for determining path of
+    # where the exe is can use it to get path to mdb files if in same folder
+    try:
+        working_dir_abs = os.path.abspath(directory)
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_name))
 
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot read "{file_name}" as it is outside the permitted working directory'
 
+        target_isfile = os.path.isfile(target_path)
+        if not target_isfile:
+            return f'Error: "{file_name}" is not a file'
 
+        return target_path
+    except Exception as e:
+        return f"Error: {e}"
 
 
 def main() -> None:
-
+    mdb_filename: str = get_mdb_filename()
+    #file_path = get_file_path(MDB_DIR_PATH, mdb_filename)
+    file_path: str = get_file_path(MDB_DIR_PATH, "test_file.txt")
+    print(file_path, "filepathtest------------------------")
 
     # initialize time variables
-    last_modified_time: float = os.stat(MDB_DIR_PATH + "test_file.txt").st_mtime
-    modified_time = last_modified_time
+    last_modified_time: float = os.stat(file_path).st_mtime
+    modified_time: float = last_modified_time
 
 
-    client = opcua_connect(OPCUA_URL)
+    client: Client = opcua_connect(OPCUA_URL)
     while client is not None:
         client = opcua_connect(OPCUA_URL)
         time.sleep(5)
@@ -72,41 +91,32 @@ def main() -> None:
         ###if write_complete == 1:
            ### pass
             #write some shenans here for reading file modify time
-        modified_time = os.stat(MDB_DIR_PATH + "test_file.txt").st_mtime
+        modified_time = os.stat(file_path).st_mtime
         if modified_time != last_modified_time:
             last_modified_time = modified_time
             #write_detected = client.get_node(WRITE_DETECTED_NODE)
             #opcua_write(write_detected, 1)
 
 
-        print(MDB_DIR_PATH + "test_file.txt", modified_time)
+        print(file_path, modified_time)
 
         time.sleep(2)
-        print("test1")
+
         db = MDBParser(file_path="test_mdb.mdb")
-        print(db)
-        print("test2")
         # Get and print the database tables
-        #print(db.tables)
-        print("test3")
+        print(db.tables)
         # Get a table from the DB.
         table = db.get_table("Data")
-        print("test4")
         # Or you can use the MDBTable class.
         #table = MDBTable(file_path="test_mdb.mdb", table="Data")
-        print("test5")
         # Get and print the table columns.
-        print(table.columns)
-        print("test6")
+        #print(table.columns)
         # Iterate the table rows.
-
         for row in table:
+            break
             print(row[0:11])
             print(len(row))
             break
-
-
-
 
         # "nt" means windows otherwise use "-"
 

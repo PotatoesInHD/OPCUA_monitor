3dc3f2dcdb8e5a58db2b69b666a3c33cf789adf4
potates <potates@mashed.com>
2026-09-13 00:22:42 -0400

    sweetened logs some more. got opcua talking to click plc. can read and write values. added background thread for heartbeat signal since values maintain upon d/c. update todo list

--- opcua_mon.py
+++ opcua_mon.py
@@ -1,23 +1,34 @@
-import time
-import json
 import os
+import time
 import datetime
+import threading
 import logging
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
-from opcua import Client
+from opcua import Client, Node
 # if I decide to read file contents when going to windows will have to
 # use pyodbc and Microsoft Access Database Engine
 from mdb_parser import MDBParser, MDBTable
 
-# ns=2 Exposed Tags (user-defined)
-# i=   Integer
-CONN_MONITOR_NODE = "ns=2;i=4"
-WRITE_DETECTED_NODE = "ns=2;i=5"
-FROM_PLC_WRITE_CMPLT_NODE = "ns=2;i=6"
+
+HEARTBEAT_NODE = "ns=1;i=2003"
+WRITE_DETECTED_NODE = "ns=1;i=2002"
+FROM_PLC_WRITE_CMPLT_NODE = "ns=1;i=2000"
 MDB_DIR_PATH  = "."
-OPCUA_URL = "opc.tcp://10.0.0.114:4840"
+OPCUA_URL = "opc.tcp://10.0.0.129:4840"
+
+# background thread
+def heartbeat_opcua(node: Node, interval=3.0):
+    state = False
+    while True:
+        try:
+            state = not state
+            node.set_value(state)
+        except Exception as err:
+            logging.warning("heartbeat_opcua() - Error: ", err)
+        time.sleep(interval)
+
 
 # setup logging
 logger = logging.getLogger()
@@ -25,7 +36,7 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s %(message)s')
 info_handler = RotatingFileHandler(
     'Info_Logs.log',
-    maxBytes=10000,
+    maxBytes=1_000_000,
     backupCount=1,
     encoding='utf-8',
 )
@@ -38,7 +49,7 @@ logger.addHandler(info_handler)
 # err handler - logs anything >= WARNING
 err_handler = RotatingFileHandler(
     'Error_Logs.log',
-    maxBytes=10000,
+    maxBytes=1_000_000,
     backupCount=1,
     encoding='utf-8',
 )
@@ -55,17 +66,20 @@ def opcua_connect(url: str)-> "Client":
         logging.info("Client connected")
         return client
     except TimeoutError as err:
-        logging.warning(f"Error: %s...Retrying connection", err)
+        logging.warning("opcua_connect() - Error: %s...Retrying connection", err)
     except Exception as err:
-        logging.warning(f"opcua_connect() - Unexpected Error: %s...Retrying connection", err)
+        logging.warning("opcua_connect() - Unexpected Error: %s...Retrying connection", err)
 
 
-def opcua_read(node) -> int:
+def opcua_read(node: Node) -> int:
     return node.get_value()
 
 
-def opcua_write(node, value: int) -> None:
-    node.set_value()
+def opcua_write(node: Node, value: int | bool) -> None:
+    try:
+        node.set_value(value)
+    except Exception as err:
+        logging.warning("opcua_write() - Error: ",err)
 
 
 def get_mdb_filename() -> str:
@@ -105,17 +119,22 @@ def main() -> None:
     # file_path = get_file_path(MDB_DIR_PATH, mdb_filename)
     # swap these file_path = later.
     file_path: str = get_file_path(MDB_DIR_PATH, "test_file.txt")
-    print(file_path, "filepathtest------------------------")
+    #print(file_path, "filepathtest------------------------")
 
     # initialize time variables
     last_modified_time: float = os.stat(file_path).st_mtime
     modified_time: float = last_modified_time
 
-
     client: Client = opcua_connect(OPCUA_URL)
-    while client is not None:
+    while client is None:
         client = opcua_connect(OPCUA_URL)
         time.sleep(5)
+
+    press_write_complete_node = client.get_node(FROM_PLC_WRITE_CMPLT_NODE)
+    heartbeat_node = client.get_node(HEARTBEAT_NODE)
+    # Start heartbeatr thread in the background:
+    t = threading.Thread(target=heartbeat_opcua, args=(heartbeat_node, 3.0), daemon=True)
+    t.start()
 
     while True:
         # creates mdb database file name based on date.
@@ -123,28 +142,27 @@ def main() -> None:
 
         # Sends val 1 to PLC while connected.
         # note for future me - need to make sure this val goes to 0 in plc when disconnected
-        ###conn_monitor = client.get_node(CONN_MONITOR_NODE)
-        ###opcua_write(conn_monitor, 1)
 
-        ###write_complete = opcua_read(FROM_PLC_WRITE_CMPLT_NODE)
-        ###if write_complete == 1:
-           ### pass
-            #write some shenans here for reading file modify time
+
+
+        press_write_complete = opcua_read(press_write_complete_node)
+
         modified_time = os.stat(file_path).st_mtime
         if modified_time != last_modified_time:
             last_modified_time = modified_time
-            logging.info("File: %s, last modified %s", mdb_filename, last_modified_time)
+            date_st_mtime = datetime.datetime.fromtimestamp(last_modified_time)
+            logging.info("File: %s, last modified = %s", mdb_filename, date_st_mtime)
             #write_detected = client.get_node(WRITE_DETECTED_NODE)
             #opcua_write(write_detected, 1)
 
 
-        print(file_path, modified_time)
+        #print(file_path, modified_time)
 
         time.sleep(2)
 
         db = MDBParser(file_path="test_mdb.mdb")
         # Get and print the database tables
-        print(db.tables)
+        ##print(db.tables)
         # Get a table from the DB.
         table = db.get_table("Data")
         # Or you can use the MDBTable class.
